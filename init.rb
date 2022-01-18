a4584caa77fb9c2e2f36aea58e37096bafdce899
Philipp Pixel <boris.schaa@cloudogu.com>
2022-01-18 09:00:22 +0100

    Add initial code
    This code was primarily generated within Redmine using the command
    ```
    RAILS_ENV="production" bundle exec rails generate redmine_plugin RedmineNoopPlugin
    ```
    
    I only added the `.gitkeep` files and modified comments within `init.rb` and
    `README.rdoc`.

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_noop_plugin do
+  name 'Redmine Noop Plugin plugin'
+  author 'Boris Schaa (boris.schaa@cloudogu.com)'
+  description 'A do-nothing plug-in used to test the plug-in (de-)installation process'
+  version '0.0.1'
+  url 'http://github.com/cloudogu/redmine-noop-plugin/'
+  author_url 'http://github.com/cloudogu/'
+end
